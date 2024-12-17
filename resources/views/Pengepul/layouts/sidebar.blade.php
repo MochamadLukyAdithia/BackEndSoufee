1bdafc2bd769951253170d84e779d5fb54d26485
Mochamad Luky Adithia <echo.mochamadluky22@gmail.com>
2024-12-17 08:37:56 +0700

    fix sidebar

--- resources/views/Pengepul/layouts/sidebar.blade.php
+++ resources/views/Pengepul/layouts/sidebar.blade.php
@@ -195,12 +195,12 @@
               </a>
           </li><!-- End Profile Page Nav -->
 
-          <li class="nav-item">
-              <a class="nav-link collapsed" href="{{ 'users' }}">
-                  <i class="bi bi-question-circle"></i>
-                  <span>User</span>
-              </a>
-          </li><!-- End F.A.Q Page Nav -->
+            {{-- <li class="nav-item">
+                <a class="nav-link collapsed" href="{{ 'users' }}">
+                    <i class="bi bi-question-circle"></i>
+                    <span>User</span>
+                </a>
+            </li><!-- End F.A.Q Page Nav --> --}}
 
           <li class="nav-item">
               <a class="nav-link collapsed" href="{{ 'kopis' }}">
